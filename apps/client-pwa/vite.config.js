83569af7dcf6af01e7abdfc6c32038011f23e532
charly <charlypolley28@gmail.com>
2026-02-04 17:04:55 +0100

    fix: force deployment by updating vite config

--- apps/client-pwa/vite.config.js
+++ apps/client-pwa/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
+// Checkpoint: Deployment fix 2026-02-04
 export default defineConfig({
 
     plugins: [
